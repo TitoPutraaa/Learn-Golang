34f4c3829b285cbcc7da27313b44b9bfcb295818
Tito Putra Bamulo <titobamulo13@gmail.com>
2026-01-27 21:09:48 +0800

    swith

--- basic/condition.go
+++ basic/condition.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	name := "tito"
+	fmt.Println(name)
 	// if statement
 	fmt.Println("---If statement---")
 	if name == "tito" {
@@ -14,12 +15,35 @@ func main() {
 		fmt.Println("correct else")
 	}
 
-	if nLength := len(name); nLength <= 5 {
+	if nLength := len(name); nLength <= 5 { //short
 		fmt.Println("name is short")
 	}else {
 		fmt.Println("long name")
 	}
 
+	// switch
 	fmt.Println("---Switch statement---")
-	fmt.Println("---Loop statement---")
+	switch name {
+	case "tito":
+		fmt.Println("the name tito")
+	case "budi": 
+		fmt.Println("the name budi")
+	default : 
+		fmt.Println("unknown name")
+	} 
+
+
+	switch cnLen := len(name); cnLen <= 5 {
+	case true : 
+			fmt.Println("short name")
+	case false : 
+		fmt.Println("long name")
+	}
+
+	// fmt.Println("---Loop statement---")
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("index ", i)
+	// }
+
+	// for i; name 
 }
