146cfc611e30aac404b2b93c4e288477cf72a065
Tito Putra Bamulo <titobamulo13@gmail.com>
2026-01-31 00:36:33 +0800

    panic function

--- basic/defer.go
+++ basic/defer.go
@@ -3,15 +3,20 @@ package main
 import "fmt"
 
 // defer : get execute after function complated, if that function error defer function still get execute
+// panic : use for stop the program
 func dMust() {
 	fmt.Println("defer function get execute")
 }
 
-func runApp() {
-	defer dMust()
+func runApp(error bool) {
+	defer dMust() // if error true still get excute
+	if error {
+		panic("ups ERROR")
+	}
 	fmt.Println("running the Application")
 }
 
+
 func main() {
-	runApp()
+	runApp(true)
 }
